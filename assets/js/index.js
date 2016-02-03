8524f70298d41c1ecf8ca743be6a08b1f7f74147
geezhawk <jonathan.cox.c@gmail.com>
2016-02-03 10:43:30 +0700

    ajax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,44 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var Hello = React.createClass({
-	render: function() {
-		return (
-			<h1>
-			Hello, React!
-			</h1>
-		)
-	}
+var BooksList = React.createClass({
+    loadBooksFromServer: function(){
+        $.ajax({
+            url: this.props.url,
+            datatype: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this)
+        })
+    },
+
+    getInitialState: function() {
+        return {data: []};
+    },
+
+    componentDidMount: function() {
+        this.loadBooksFromServer();
+        setInterval(this.loadBooksFromServer, 
+                    this.props.pollInterval)
+    }, 
+    render: function() {
+        if (this.state.data) {
+            console.log('DATA!')
+            var bookNodes = this.state.data.map(function(book){
+                return <li> {book.title} </li>
+            })
+        }
+        return (
+            <div>
+                <h1>Hello React!</h1>
+                <ul>
+                    {bookNodes}
+                </ul>
+            </div>
+        )
+    }
 })
 
-ReactDOM.render(<Hello />, document.getElementById('container'))
+ReactDOM.render(<BooksList url='/api/' pollInterval={1000} />, 
+    document.getElementById('container'))
